9247c3d0224921a3a72a4e3e4ff3c04045e5727b
Shahan <malikshahannavid@gmail.com>
2023-05-12 15:58:18 +0500

    Hello world

--- src/main/kotlin/FirstProgram.kt
+++ src/main/kotlin/FirstProgram.kt
@@ -1,16 +1,21 @@
-
 var fname: String = "Malik"
 fun main() {
 
     /*  println("Hello World")
    */
-    /*
+
     var name = "Jameel"
     val birthYear = 1984
     println(name)
     println(birthYear)
-*/
     println(fname)
- fname = "shahan"
-    print(fname)
+    val lastName = "Shahan"
+    println("Hello " + fname)
+    println("your full name is " + fname + " " + lastName)
+
+    val pi = 3.141592
+    var diameter = 12.25
+    var area: Double
+    area = pi * diameter
+    println("Area of " + diameter + " is " + area)
 }
