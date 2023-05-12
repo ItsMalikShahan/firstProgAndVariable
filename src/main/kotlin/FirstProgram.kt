4b632dd0b7ae526fa0ceed5b35c0c8eafc6ed943
Shahan <malikshahannavid@gmail.com>
2023-05-12 15:43:22 +0500

    Hello world

---
+++ src/main/kotlin/FirstProgram.kt
@@ -0,0 +1,16 @@
+
+var fname: String = "Malik"
+fun main() {
+
+    /*  println("Hello World")
+   */
+    /*
+    var name = "Jameel"
+    val birthYear = 1984
+    println(name)
+    println(birthYear)
+*/
+    println(fname)
+ fname = "shahan"
+    print(fname)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+
+var name: String? = null
+val lastName: String = "malik"
+fun main() {
+  //  println("am Malikf")
+    name = "Kashif"
+    if (name != null){
+        println(name)
+    }
+//    println(name);
+    println(lastName)
+
+//    println(name)
+
+}
