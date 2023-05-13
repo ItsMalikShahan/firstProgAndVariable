a4ca2d16641c294abfab7a8a73b98487a1719e0a
Shahan <malikshahannavid@gmail.com>
2023-05-13 14:47:33 +0500

    Data Types

---
+++ src/main/kotlin/dataTypes.kt
@@ -0,0 +1,20 @@
+
+fun main (){
+    /*
+    In this program we will learn how we can use different
+    data types in our kotlin project and will write a program to
+    through which we can get student data
+     */
+    var firstName = "Shahan"   // String
+    var lastName = "Naveed"    // String
+    val rollNo = 52            // Int
+    val grade = 'B'            // Char
+    val amount = 2300.00       // Double
+    val isDaySchalor = true
+
+    println("Hello my name is " +firstName)
+    println("My registration number is " +rollNo)
+    println("My grade in last semester is " + grade)
+    println("Amount i have in my wallet is " + amount)
+    println("Are you daySchalor " + isDaySchalor)
+}
